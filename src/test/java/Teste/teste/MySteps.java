b92230df8bf3f486e052bcfd8c92c5cb70193368
eduardo.mesquita <eduardo.mesquita@avancoinfo.com.br>
2016-05-03 14:14:00 -0300

    [1.0]primeiro teste com dbehave

---
+++ src/test/java/Teste/teste/MySteps.java
@@ -0,0 +1,9 @@
+package Teste.teste;
+
+import br.gov.frameworkdemoiselle.behave.parser.Step;
+import br.gov.frameworkdemoiselle.behave.parser.jbehave.CommonSteps;
+
+public class MySteps extends CommonSteps implements Step {
+	
+
+}
