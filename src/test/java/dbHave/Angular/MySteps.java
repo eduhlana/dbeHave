b665ef7e5bb59a0704a88a06181c5a48568f7c34
eduardo.mesquita <eduardo.mesquita@avancoinfo.com.br>
2016-05-05 11:00:24 -0300

    dbe[02]Adcionado item ao pacote

---
+++ src/test/java/dbHave/Angular/MySteps.java
@@ -0,0 +1,8 @@
+package dbHave.Angular;
+
+import br.gov.frameworkdemoiselle.behave.parser.Step;
+import br.gov.frameworkdemoiselle.behave.parser.jbehave.CommonSteps;
+
+public class MySteps extends CommonSteps implements Step {
+
+}
