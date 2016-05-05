6f3983ad256269d43cd923148c351ad95d372fbd
eduardo.mesquita <eduardo.mesquita@avancoinfo.com.br>
2016-05-05 10:09:12 -0300

    dbh[01] Funcionando

--- src/test/java/Teste/teste/MySteps.java
+++
@@ -1,9 +0,0 @@
-package Teste.teste;
-
-import br.gov.frameworkdemoiselle.behave.parser.Step;
-import br.gov.frameworkdemoiselle.behave.parser.jbehave.CommonSteps;
-
-public class MySteps extends CommonSteps implements Step {
-	
-
-}
