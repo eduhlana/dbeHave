4b17c699c5cfe705ea68465b18105e08cb6dca59
eduhlana <mesquitalana@gmail.com>
2016-06-21 19:03:49 -0300

    []TDD

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,8 +3,29 @@ public class Calculadora {
 
 	public int soma(String numeros) {
 		
-		// Aqui será implementado o método soma.
-		return -1;
+		if (numeros.length() == 0){
+			// Aqui será implementado o método soma.
+			return 0;
+		}
+		else{
+			 
+			String [] vetor = numeros.split("[\n,]");
+			if(vetor.length == 1)
+			{
+				return Integer.parseInt(numeros);
+			}
+			else{
+				int vlrTotal = 0;
+				
+				for (int i=0; vetor.length > i; i++)
+				{
+					vlrTotal += Integer.parseInt(vetor[i]);
+				}
+				
+				return vlrTotal;
+			}
+		}
+
 		
 	}
 	
--- src/TestaCalculadora.java
+++ src/TestaCalculadora.java
@@ -6,7 +6,8 @@ public class TestaCalculadora {
 	// Essa anotação indica ao JUnit que esse método é um caso de teste. Para executar
 	// os testes da classe basta clicar com o botão direito na mesma e escolher:
 	// "Run As" > "JUnit Test"
-	@Test
+	
+	@Test //passo 1
 	public void testaComStringVazia() {
 		Calculadora c = new Calculadora();
 		
@@ -20,5 +21,38 @@ public class TestaCalculadora {
 //			Assert.fail("A saída deveria ser 0, mas é " + saida);
 //		}
 	}
+	@Test//passo 2
+		public void testaStringComNumeroUnico() {
+			Calculadora c = new Calculadora();
+			
+			
+			// É esperado que saída seja um número.
+			// usando a biblioteca JUnit:
+			Assert.assertEquals(1, c.soma("1"));
+			Assert.assertEquals(44, c.soma("44"));
+			Assert.assertEquals(21, c.soma("21"));
+	}
 	
+	@Test//passo 3
+	public void testaStringQtdeQualquer() {
+		Calculadora c = new Calculadora();
+		
+		
+		// É esperado que saída seja a soma dos valores inseridos.
+		// usando a biblioteca JUnit:
+		Assert.assertEquals(20, c.soma("9,1,10"));
+		Assert.assertEquals(34, c.soma("10,14,10"));
+		Assert.assertEquals(-2, c.soma("2,-4"));
+}
+	@Test//passo 4
+	public void QuebraLinha() {
+		Calculadora c = new Calculadora();
+		
+		
+		// É esperado que saída seja a soma dos valores inseridos.
+		// usando a biblioteca JUnit:
+		Assert.assertEquals(20, c.soma("9\n1\n10"));
+		Assert.assertEquals(34, c.soma("10\n14\n10"));
+		Assert.assertEquals(-2, c.soma("2\n-4"));
+}
 }
